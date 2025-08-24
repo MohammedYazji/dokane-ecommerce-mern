1dd4fe9260d7db67feb3cf814946fdcdb61ad092
Mohammed Yazji <mw.alyazji@gmail.com>
2025-08-24 10:09:10 +0300

    implement global error handling middleware implement global error handling middleware with simple errors instances without custom error class

--- server/index.js
+++ server/index.js
@@ -23,9 +23,25 @@ app.use("/api/v1/auth", authRoutes);
 // handle unhandled routes for all methods
 // this will run if not catch in any route before
 app.use((req, res, next) => {
-  res.status(404).json({
-    status: "fail",
-    message: `Can't find ${req.originalUrl} on this server!`,
+  // res.status(404).json({
+  //   status: "fail",
+  //   message: `Can't find ${req.originalUrl} on this server!`,
+  // });
+
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = "fail";
+  err.statusCode = 404;
+  next(err); // in express anything pass in next will consume as an error
+});
+
+// make a global error handling middleware
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
