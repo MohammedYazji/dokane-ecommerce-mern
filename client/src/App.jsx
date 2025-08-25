e2518647df8bb563ba7d99d8608308b8bc45c47c
Mohammed Yazji <mw.alyazji@gmail.com>
2025-08-25 15:10:24 +0300

    setup frontend with vite and tailwind and install required dependencies

---
+++ client/src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return <div className="text-amber-300 font-bold">Ahmad mosh Ahbal</div>;
+}
+
+export default App;
+
+// setup frontend with vite and taiwlind and install reuired packages
