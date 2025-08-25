8f6731473486420f9b0c298671b0f973691878a0
Mohammed Yazji <mw.alyazji@gmail.com>
2025-08-25 05:18:27 +0300

    some fixes in product routes folder

--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -12,12 +12,15 @@ import { adminRoute, protectRoute } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
+// admin-only
 router.get("/", protectRoute, adminRoute, getAllProducts);
+router.post("/", protectRoute, adminRoute, createProduct);
+router.put("/:id", protectRoute, adminRoute, toggleFeatured);
+router.delete("/:id", protectRoute, adminRoute, deleteProduct);
+
+// public
 router.get("/featured", getFeaturedProducts);
 router.get("/recommendations", getRecommendedProducts);
 router.get("/category/:cat", getProductsByCategory);
-router.post("/", protectRoute, adminRoute, createProduct);
-router.get("/:id", toggleFeatured);
-router.post("/:id", protectRoute, adminRoute, deleteProduct);
 
 export default router;
